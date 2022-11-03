4890b7fd3d23b1dc2a08d293e6596e285d446e72
Atte Kivistö <atte.t.kivisto@gmail.com>
2022-11-03 14:25:06 +0200

    1.3

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,33 +1,40 @@
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
- 
-    const Content = (props) => {
-      return (
-        <div>
-          <Part part={props.part1} exercise={props.exercise1} />
-          <Part part={props.part2} exercise={props.exercise2} />
-          <Part part={props.part3} exercise={props.exercise3} /> 
-        </div>
-      )
-    }
-
-    const Header = ({course}) => <h1>{course}</h1>;
-    const Part = ({part, exercise}) => <p>{part} {exercise}</p>;
-    const Total = (props) => <p>Number of exercises {props.sum}</p>;
   
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  
+  const Content = (props) => {
     return (
       <div>
-        <Header course={course} />
-        <Content part1={part1} part2={part2} part3={part3} exercise1={exercises1} exercise2={exercises2} exercise3={exercises3}/>
-        <Total sum={exercises1 + exercises2 + exercises3}/>
+        <Part part={props.part1.name} exercise={props.part1.exercises} />
+        <Part part={props.part2.name} exercise={props.part2.exercises} />
+        <Part part={props.part3.name} exercise={props.part3.exercises} /> 
       </div>
     )
   }
+
+  const Header = ({course}) => <h1>{course}</h1>;
+  const Part = ({part, exercise}) => <p>{part} {exercise}</p>;
+  const Total = (props) => <p>Number of exercises {props.sum}</p>;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total sum={part1.exercises + part2.exercises + part3.exercises}/>
+    </div>
+  )
+}
   
   export default App
