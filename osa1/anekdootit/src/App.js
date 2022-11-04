f3ab1ae497f0181ad5bfec87fe74bdd6baf0ac06
Atte Kivistö <atte.t.kivisto@gmail.com>
2022-11-04 19:48:55 +0200

    anekdootit ready 1.13-1.14

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -10,18 +10,45 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
-   
-  const [selected, setSelected] = useState(0)
+  
+  const [selected, setSelected] = useState(Math.floor(Math.random() * anecdotes.length))
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
   const buttonHandler = () => setSelected(Math.floor(Math.random() * anecdotes.length))
 
-  const Button = ({handler}) => <button onClick={handler}>Anecdote</button>
+  const voteHandlder = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    return (
+      setVotes(newVotes)
+    )}
+
+  const Button = ({name, handler}) => <button onClick={handler}>{name}</button>
   const Anecdote = ({anecdote}) => <p>{anecdote}</p>
-  
+  const AnecdoteVoteCount = ({votes}) => <p>has {votes} votes</p>
+  const AnecdoteMostVotes = ({anecdotes, votes}) => {
+    let mostVotes = 0
+    for (let i = 1; i < anecdotes.length; i++) {
+      if (votes[i] > votes[mostVotes]) mostVotes = i
+    }
+
+    return (
+      <>
+        <Anecdote anecdote={anecdotes[mostVotes]} />
+        <AnecdoteVoteCount votes={votes[mostVotes]} />
+      </>
+    )}
+
   return (
     <>
-      <Button handler={buttonHandler}/>
-      <Anecdote anecdote={anecdotes[selected]}/>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdote={anecdotes[selected]} />
+      <AnecdoteVoteCount votes={votes[selected]} />
+      <Button name="vote" handler={voteHandlder}/>
+      <Button name="next anecdote" handler={buttonHandler}/>
+      <h1>Anecdote with most votes</h1>
+      <AnecdoteMostVotes anecdotes={anecdotes} votes={votes}/>
+
     </>
   )
 }
