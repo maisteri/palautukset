9bbbf336303a5a6b1964a5fffe51d9df7fc4aad8
Atte Kivistö <atte.t.kivisto@gmail.com>
2022-12-01 12:08:31 +0200

    2.14 ready

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import api_key from './constants'
 
 const Filter = ({countryFilter,handleCountryFilterChange}) => {
   return (
@@ -16,7 +17,7 @@ const Country = (props) => {
   const area = country.area
   const languages = country.languages
   const flagUrl = country.flags.svg
-  console.log(languages)
+
   return (
     <>
       <h1>{name}</h1>
@@ -28,27 +29,49 @@ const Country = (props) => {
       <br/>
       {Object.values(languages).map(x => <li key={x}> {x} </li>)}
       <br/>
-      <img src={flagUrl} alt="country flag" width="50%" height="50%"></img>
+      <img src={flagUrl} alt="country flag" width="25%" height="25%"></img>
     </>
   )
 }
 
-const Result = ({countries, countryFilter, setCountryFilter}) => {
+const Weather = (props) => {
 
-  const countryFilterFunction = country => {
+  const [weather, setWeather] = useState({})
+
+  const country = props.country[0]
+  const capitalLat = country.capitalInfo.latlng[0]
+  const capitalLng = country.capitalInfo.latlng[1]
+  const capital = country.capital
+
+  const part = "minutely,hourly,daily,alerts"
+
+  useEffect( () => {
+    axios
+      .get(`https://api.openweathermap.org/data/3.0/onecall?lat=${capitalLat}&lon=${capitalLng}&exclude=${part}&appid=${api_key}&units=metric`)
+      .then(response => setWeather(response.data))
+  }, [capitalLat, capitalLng])
+  
+  if (weather.current) {
     return (
-      country.name.common.toLowerCase().includes(countryFilter.toLowerCase())
+      <>
+        <h2>Weather in {capital}</h2>
+        <p>temperature {weather.current.temp} Celsius</p>
+        <img src={`http://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`} alt={weather.current.weather[0].description} width="25%" height="25%"></img>
+        <p>wind {weather.current.wind_speed} m/s</p>
+      </>
     )
+  } else {
+    return null
   }
+}
 
-  const filteredCountries = countries.filter(countryFilterFunction)
+const Result = ({filteredCountries, setCountryFilter}) => {
   
   if ( filteredCountries.length > 10 ) {
     return (
       <p>Too many matches, specify another filter</p>
     )
   } else if ( filteredCountries.length > 1 || filteredCountries.length === 0 ) {
-    
     return (
       filteredCountries.map(x => {
         return (
@@ -60,9 +83,11 @@ const Result = ({countries, countryFilter, setCountryFilter}) => {
       })
     )
   } else {
-    
     return (
-      <Country country={filteredCountries} />
+      <>
+        <Country country={filteredCountries} />
+        <Weather country={filteredCountries} />
+      </>
     )
   }
 }
@@ -73,21 +98,25 @@ const App = () => {
   const [countries, setCountries] = useState([])
   
   useEffect( () => {
-    console.log("Effect used. Nice!")
     axios
       .get('https://restcountries.com/v3.1/all')
       .then( response => setCountries(response.data) )
   }, [])
 
   const handleCountryFilterChange = (event) => setCountryFilter(event.target.value)
+  
+  const countryFilterFunction = country => {
+    return (
+      country.name.common.toLowerCase().includes(countryFilter.toLowerCase())
+    )
+  }
+  const filteredCountries = countries.filter(countryFilterFunction)
 
-  console.log(countries.length)
   return (
     <>
       <Filter countryFilter={countryFilter}
               handleCountryFilterChange={handleCountryFilterChange} />
-      <Result countries={countries} 
-              countryFilter={countryFilter}
+      <Result filteredCountries={filteredCountries}
               setCountryFilter={setCountryFilter} />
     </>
   )
