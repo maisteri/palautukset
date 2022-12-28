05d787c2b02c6107051c699452d2e00b26b3ac5f
Atte Kivistö <atte.t.kivisto@gmail.com>
2022-12-28 16:11:59 +0200

    Blogilista: 4.2 done

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogsRouter')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error in connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,19 +1,10 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogsRouter')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
