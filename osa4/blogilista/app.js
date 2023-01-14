714d3fcd398c02b91527ebae2dca9e55617762f9
Atte Kivistö <atte.t.kivisto@gmail.com>
2023-01-14 13:20:36 +0200

    4.15

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogsRouter')
+const usersRouter = require('./controllers/usersRouter')
 const middleware = require('./utils/middleware')
+require('express-async-errors')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -20,6 +22,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -1,17 +1,17 @@
 const logger = require('./logger')
 
 const errorHandler = (error, request, response, next) => {
-    logger.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  module.exports = {
-    errorHandler
-  }
+  next(error)
+}
+
+module.exports = {
+  errorHandler
+}
