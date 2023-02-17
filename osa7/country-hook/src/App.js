2821dc811f6276a1b260eb90c254c8b5768721f4
Atte Kivistö <atte.t.kivisto@gmail.com>
2023-02-17 07:43:50 +0200

    up to 7.8

---
+++ osa7/country-hook/src/App.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    const countryName = name 
+    const baseUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
+      
+    if (name) {
+      axios.get(baseUrl)
+        .then(response => response.data)
+        .then(res => {
+          setCountry({
+            found: true,
+            data: {
+              capital: res[0].capital[0],
+              population: res[0].population,
+              name: res[0].name.common,
+              flag: res[0].flags.png
+            }
+          })
+        })
+        .catch(
+          () => console.log('faulty country name')
+        )
+      }
+    }, [name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
